613d0b35c3ecf0a2f5e49bc26a4601d5ab109caa
Jinam Shah <jinams47@gmail.com>
2025-04-11 13:01:46 -0400

    cvat - fixed config

--- setup_cvat.sh
+++ setup_cvat.sh
@@ -6,6 +6,9 @@ cd ../
 git clone https://github.com/cvat-ai/cvat.git
 cd cvat
 
+# need to set host to public IP address
+# do everytime cvat is started
+export CVAT_HOST=sunny.ece.ncsu.edu
 # needs platform specification if not running on x86_64
 # DOCKER_DEFAULT_PLATFORM=linux/amd64 docker compose up -d
 docker compose up -d
